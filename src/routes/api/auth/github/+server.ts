53a0a8fbeeda54411d5dc0cdd3f7544651b170ba
naimulh247 <naimulh247@gmail.com>
2025-01-03 12:45:57 -0500

    fix: use env PUBLIC_URL

--- src/routes/api/auth/github/+server.ts
+++ src/routes/api/auth/github/+server.ts
@@ -1,11 +1,13 @@
 import { GITHUB_CLIENT_ID } from '$env/static/private';
+import { PUBLIC_URL } from '$env/static/public';
 
 export async function GET() {
+  console.log(PUBLIC_URL)
   const state = crypto.randomUUID();
   
   const params = new URLSearchParams({
     client_id: GITHUB_CLIENT_ID,
-    redirect_uri: 'http://localhost:5173/api/auth/callback',
+    redirect_uri: PUBLIC_URL + '/api/auth/callback',
     scope: 'repo delete_repo',
     state
   });
